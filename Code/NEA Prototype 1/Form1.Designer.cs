7e00e699b5b3f86d7eb881b37388f52e65576a6b
femboycoding <kilianmarzella@outlook.com>
2022-06-01 11:45:00 +0100

    Began coding

---
+++ Code/NEA Prototype 1/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace NEA_Prototype_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTillTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTillTitle
+            // 
+            this.lblTillTitle.AutoSize = true;
+            this.lblTillTitle.Location = new System.Drawing.Point(355, 63);
+            this.lblTillTitle.Name = "lblTillTitle";
+            this.lblTillTitle.Size = new System.Drawing.Size(22, 15);
+            this.lblTillTitle.TabIndex = 0;
+            this.lblTillTitle.Text = "Till";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTillTitle);
+            this.Name = "Form1";
+            this.Text = "Till";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTillTitle;
+    }
+}
+
