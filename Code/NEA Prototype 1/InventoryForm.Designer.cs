9ef5aaa4db820788756dbc3ee7ec6012ec78014f
femboycoding <kilianmarzella@outlook.com>
2022-07-22 12:23:05 +0100

    Sales and inventory look better
    Added columns and rearranged

--- Code/NEA Prototype 1/InventoryForm.Designer.cs
+++ Code/NEA Prototype 1/InventoryForm.Designer.cs
@@ -31,35 +31,81 @@ namespace NEA_Prototype_1
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnLoginForm = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ItemType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.ItemType,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(10, 9);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(300, 188);
+            this.dataGridView1.Size = new System.Drawing.Size(647, 141);
             this.dataGridView1.TabIndex = 0;
             // 
             // btnLoginForm
             // 
-            this.btnLoginForm.Location = new System.Drawing.Point(677, 42);
+            this.btnLoginForm.Location = new System.Drawing.Point(283, 197);
+            this.btnLoginForm.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLoginForm.Name = "btnLoginForm";
-            this.btnLoginForm.Size = new System.Drawing.Size(94, 29);
+            this.btnLoginForm.Size = new System.Drawing.Size(82, 22);
             this.btnLoginForm.TabIndex = 1;
             this.btnLoginForm.Text = "Login Form";
             this.btnLoginForm.UseVisualStyleBackColor = true;
             this.btnLoginForm.Click += new System.EventHandler(this.btnLoginForm_Click);
             // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ItemID";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "ItemPrice";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "ItemStock";
+            this.Column3.Name = "Column3";
+            // 
+            // ItemType
+            // 
+            this.ItemType.HeaderText = "ItemType";
+            this.ItemType.Name = "ItemType";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "ItemSupplier";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "ItemSize";
+            this.Column5.Name = "Column5";
+            // 
             // InventoryForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 338);
             this.Controls.Add(this.btnLoginForm);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "InventoryForm";
             this.Text = "InventoryForm";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -71,5 +117,11 @@ namespace NEA_Prototype_1
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btnLoginForm;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ItemType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
     }
 }
